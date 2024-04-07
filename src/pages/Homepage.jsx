a8141cf1b9b92e1ed63c622d5203f5b255490e3a
Danilim04 <daniel.ferraz04@hotmail.com>
2024-04-06 23:37:03 -0300

    firstCommitFeature

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import React from 'react';
+import Header from '../components/Header/index'
+import Home from '../components/Home'
+import Aboutus from '../components/Aboutus'
+import Testimonial from '../components/Testimonial'
+
+const HomePage = () => {
+    return (
+      <>
+        {/*Aqui são chamados os componentes da Homepage*/}
+        <Header />
+        <Home />
+        <div id='aboutus'><Aboutus /></div>
+        <Testimonial />
+      </>
+    )
+  }
+export default HomePage
